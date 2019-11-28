d3157428c66cb2f23830db74cdb3be34394e1e17
mofo7777 <41967574+mofo7777@users.noreply.github.com>
2019-11-28 01:51:13 +0100

    Add files via upload

---
+++ DirectXVideoScreen/D3D11ShaderNV12/CombinedUVPS.hlsl
@@ -0,0 +1,27 @@
+//--------------------------------------------------------------------------------------
+// CombinedUVPS.hlsl
+//--------------------------------------------------------------------------------------
+Texture2D txInputU : register(t0);
+Texture2D txInputV : register(t1);
+Texture1D txInputShift : register(t2);
+
+SamplerState GenericSampler : register(s0);
+
+struct PS_INPUT
+{
+	float4 Pos : SV_POSITION;
+	float2 Tex : TEXCOORD;
+};
+
+//--------------------------------------------------------------------------------------
+// Pixel Shader
+//--------------------------------------------------------------------------------------
+float PS(PS_INPUT input) : SV_Target
+{
+	float fShift = txInputShift.Sample(GenericSampler, input.Tex.x);
+
+	if(fShift == 0.0f)
+		return txInputU.Sample(GenericSampler, input.Tex);
+	else
+		return txInputV.Sample(GenericSampler, input.Tex);
+}
